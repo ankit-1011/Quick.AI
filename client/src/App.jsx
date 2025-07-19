6233824b5ed0ed5036ffa60c6f588aa279161b6a
ankit mishra <ankitmishra16127@gmail.com>
2025-07-19 20:13:00 +0530

    added Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import Home from './pages/Home'
 import Layout from './pages/Layout'
 import BlogTitles from './pages/BlogTitles'
 import WriteArticle from './pages/WriteArticle'
+import GenerateImage from './pages/GenerateImages'
+import RemoveObject from './pages/RemoveObject'
+import {Community} from './pages/Community'
+import RemoveBackground from './pages/RemoveBackground'
+import ReviewResume from './pages/ReviewResume'
 
 const App = () => {
   return (
@@ -14,6 +19,11 @@ const App = () => {
             <Route index element={<Layout/>}/>
             <Route path='write-article' element={<WriteArticle/>}/>
             <Route path='blog-titles' element={<BlogTitles/>}/>
+            <Route path='generate-images' element={<GenerateImage/>}/>
+            <Route path='remove-background' element={<RemoveBackground/>}/>
+            <Route path='remove-object' element={<RemoveObject/>}/>
+            <Route path='review-resume' element={<ReviewResume/>}/>
+            <Route path='community' element={<Community/>}/>
           </Route>
       </Routes>
     </div>
