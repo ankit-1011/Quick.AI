b19e98ef949936c2d5da5b8fa7005f5fcfad8ae6
ankit mishra <ankitmishra16127@gmail.com>
2025-07-26 08:51:15 +0530

    Done with Pages

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -14,14 +14,14 @@ const RemoveBackground = () => {
   return (
     <div className='h-full overflow-y-scroll p-6 flex flex-col lg:flex-row items-start gap-4 text-slate-700'>
       {/* left col */}
-      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg borderborder-gray-200'>
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
         <div className='flex items-center gap-3'>
           <Sparkles className='w-6 text-[#FF4938] ' />
           <h1 className='text-xl font-semibold' >Background Removal</h1>
         </div>
         <p className='mt-6 text-sm font-medium'>Upload image</p>
 
-        <input onChange={(e) => setInput(e.target.files[0])}  type="image/*" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'  required />
+        <input onChange={(e) => setInput(e.target.files[0])}  type="file"  accept="image/*" value={input} className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'  required />
 
         <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG , PNG and other image formats</p> 
       
