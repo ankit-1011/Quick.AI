ec532a721b33f61f540b6c76e2c8bf76eeb9da6c
ankit mishra <ankitmishra16127@gmail.com>
2025-07-31 14:27:07 +0530

    Adding the functionality

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,13 +1,40 @@
 import React, { useState } from 'react'
 import {Eraser, Hash, Sparkles} from 'lucide-react'
+import axios from 'axios'
+import { toast } from 'react-hot-toast';
+import Markdown from 'react-markdown'
+import { useAuth } from '@clerk/clerk-react';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const RemoveBackground = () => {
 
-
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+  
+    const { getToken } = useAuth()
 
   const onSubmitHandler = async (e) => {
     e.preventDefault()
+    try{
+     setLoading(true)
+
+    const formData = new FormData()
+    formData.append('image',input)
+      
+     const {data} = await axios.post('/api/ai/remove-image-background',formData,{headers : {Authorization : `Bearer ${await getToken()}`}})
+
+     if(data.success){
+      setContent(data.content)
+     }else{
+      toast.error(data.message)
+     }
+    }catch(error){
+     toast.error(error.message)
+    }
+    setLoading(false)
   }
 
 
@@ -21,12 +48,12 @@ const RemoveBackground = () => {
         </div>
         <p className='mt-6 text-sm font-medium'>Upload image</p>
 
-        <input onChange={(e) => setInput(e.target.files[0])}  type="file"  accept="image/*" value={input} className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'  required />
+        <input onChange={(e) => setInput(e.target.files[0])}  type="file"  accept="image/*"  className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'  required />
 
         <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG , PNG and other image formats</p> 
       
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-1g cursor-pointer'>
-          <Eraser className='w-5' />
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-1g cursor-pointer'>
+          {loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> : <Eraser className='w-5'/>}
           Remove Background
         </button>
       </form >
@@ -38,13 +65,21 @@ const RemoveBackground = () => {
           <h1 className='text-xl font-semibold' >Process Image</h1>
         </div>
 
-        <div className='flex-1 flex justify-center items-center'>
+       {
+        !content ?
+        (
+              <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
             <Eraser className='w-9 h-9' />
 
             <p>Upload an Image & click "Remove Backgound" to get started</p>
           </div>
         </div>
+        ):
+        (
+          <img src={content} alt='image' className='mt-3 w-full h-full' />
+        )
+       }
       </div>
     </div >
   )
